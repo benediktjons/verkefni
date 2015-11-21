74de344cc008b0777aee0a5e335bedda893af8c2
IngvarBjarki <ingvarbjarki@gmail.com>
2015-11-21 16:36:21 +0000

    setti gildi i frontpage og atti sma vid utlid baeti id og svona i odru

--- public/js/frontpage.js
+++ public/js/frontpage.js
@@ -1,20 +1,54 @@
 $(document).ready(function (){
 
-  $("a").click(function () {
+    // Herna koma gildinn ur leitinni
+    //her eru select gildinn
+    $("select, input").click(function () {
+      var leitFra = $("#leitFra").val();
+      var leitTil = $("#leitTil").val();
+      var leitTimabilFra = $("#date-picker-2").val();
+      var leitTimabilTil =$("#date-picker-3").val();
+// her eru checkbox gildinn tu fa true og false gildi
+    var leitReyklaus = $( "#leitReyklaus" ).is(':checked'); // skilar true ef tad er checkd annars false
+    var leitOskaEftirFari = $( "#leitOskaEftirFari" ).is(':checked');
+    var leitOkumen =$( "#leitOkumen:checkbox:checked" ).is(':checked');
 
-    $(this).css("text-decoration", "underline");
 
 
+    console.log(leitFra);
+    console.log(leitReyklaus);
   });
 
-  $(".date-picker").datepicker();
+/*
+$("#leitFra").click(function () {
+  var leitFra = $("#leitFra").val();
 
-  $(".date-picker").on("change", function () {
-    var id = $(this).attr("id");
-    var val = $("label[for='" + id + "']").text();
-    $("#msg").text(val + " changed");
-  });
+  if{id-id a leit fra !== leitFra}{
+    idaleitfra.hide()
+  }*/
+
+
+    // herna endar leitinn og klick underline fyror siduna sem tu ert a kemur inn
+    $("a").click(function () {
+      $(this).css("text-decoration", "underline");
+    });
+
+
+// herna endar underline fallid og date picker fyrir dagatalid kemur inn
+
+$(".date-picker").datepicker();
+
+$(".date-picker").on("change", function () {
+  var id = $(this).attr("id");
+  var val = $("label[for='" + id + "']").text();
+  $("#msg").text(val + " changed");
+});
+
+  // herna endar date picker og clockpicker kemur inn fyrir klukkuna
 
   $('.clockpicker').clockpicker();
+
+
+
+
 
 });
