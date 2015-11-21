ea108fce4124491eb51200a4cc1f9c16879534ae
benediktjons <bensi91@gmail.com>
2015-11-21 16:25:40 +0000

    færslur úr gagnagrunni birtast nú á forsíðu, en margt ábótavant ennþá

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,16 @@
 
 var express = require('express');
 var router = express.Router();
+var entries = require('../lib/entries')
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Velkominn' });
+    entries.listEntries(function (err, all){
+    if (err){
+      console.error(err);
+    }
+    res.render('index',{title: 'Velkominn',
+      entries: all});
+  });
 });
 
 module.exports = router;
