1cd713602c66369b29d1b0552c17e458a8919dad
IngvarBjarki <ingvarbjarki@gmail.com>
2015-11-24 11:59:37 +0000

    er ad seakja npm dot

--- public/js/frontpage.js
+++ public/js/frontpage.js
@@ -1,5 +1,7 @@
 $(document).ready(function (){
 
+
+
   'use strict';
   // her er til og fra gaurinn
   $("input, select").on("change click", function () {
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,15 @@
 
 var express = require('express');
 var router = express.Router();
-var entries = require('../lib/entries')
+var entries = require('../lib/entries');
 
 router.get('/', function(req, res) {
     entries.listEntries(function (err, all){
     if (err){
       console.error(err);
     }
+    console.log("her er entries")
+    console.log(entries)
     res.render('index',{title: 'Velkominn',
       entries: all});
   });
